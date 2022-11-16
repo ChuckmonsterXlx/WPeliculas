107d1787c554d368d6011ca98926469a4536c1fe
ChuckmonsterXlx <alfonso_309@hotmail.com>
2022-11-15 18:35:17 -0700

    Ya se muestra el arreglo

--- src/components/SearchArea.js
+++ src/components/SearchArea.js
@@ -5,7 +5,7 @@ const SearchArea = (props) => {
         <div className="container">
         <div className="row">
             <section className="col s4 offset-s4">
-                <form action="" onSumbit={props.handleSubmit}>
+                <form action="" onSubmit={props.handleSubmit}>
                     <div className="input-field">
                         <input placeholder="Search movie..." type="text" onChange={props.handleChange} />
                     </div>
